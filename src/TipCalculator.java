8aaaf40e889ee5530c64cbb16fcc0811b55818db
BT_2N2_14 <sample@bths.edu>
2023-09-21 14:02:08 -0400

    added base of program added while loop to program started writing some of the print statements rounded the numbers

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -10,16 +10,16 @@ public class TipCalculator {
         double perPerson;
         double perPersonTip;
         double tip = 0;
-        double percent;
+        int percent;
         double justTip;
         double price;
 
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your party? ");
         people = scan.nextInt();
-        System.out.print("What's the top percentage? (0-100): ");
-        percent = scan.nextDouble();
-        tip = percent / 100;
+        System.out.print("What's the tip percentage? (0-100): ");
+        percent = scan.nextInt();
+        tip = (double) percent / 100;
         System.out.print("Enter a cost in dollars and cents, e.g. 12.45 (-1 to end): ");
         price = scan.nextDouble();
         total+=price;
@@ -33,14 +33,22 @@ public class TipCalculator {
         }
         justTip = total * tip;
         withTip = justTip + total;
+        justTip =(int) (justTip * 100) ;
+        justTip /=100;
+        withTip =(int) (withTip * 100) ;
+        withTip /=100;
         perPersonTip = justTip / people;
-        perPersonTip*=100
+        perPersonTip =(int) (perPersonTip *100) ;
+        perPersonTip /=100;
 
         System.out.println("--------------------------------------------------");
         System.out.println("Total bill before tip: $" + total);
         System.out.println("Total percentage: " + percent + "%");
         System.out.println("Total tip: $" + justTip);
         System.out.println("Total bill with tip: $" + withTip);
+
+        System.out.println("Tip per person: $"+ perPersonTip);
+
 
         }
     }
