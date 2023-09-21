1829c648aad421497861c329ecc73a46eaf0a450
BT_2N2_14 <sample@bths.edu>
2023-09-21 13:37:19 -0400

    added base of program added while loop to program started writing some of the print statements

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -5,28 +5,40 @@ public class TipCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double total = 0;
+        double withTip;
         int people;
-        double average;
+        double perPersonWithTip;
+        double perPerson;
+        double perPersonTip;
         double tip = 0;
+        double percent;
+        double justTip;
         double price;
         System.out.println("Welcome to the tip calculator!");
-        System.out.println("How many people are in your party? ");
+        System.out.print("How many people are in your party? ");
         people = scan.nextInt();
-        System.out.println("What's the top percentage? (0-100): ");
-        tip = scan.nextDouble();
-        tip /=100;
-        tip++; // total times this = total w tip
+        System.out.print("What's the top percentage? (0-100): ");
+        percent = scan.nextDouble();
+        tip = percent / 100;
         System.out.print("Enter a cost in dollars and cents, e.g. 12.45 (-1 to end): ");
         price = scan.nextDouble();
         total+=price;
+
         while (price != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.45 (-1 to end): ");
             price = scan.nextDouble();
             if (price != -1) {
                 total+=price;
             }
+        }
+        justTip = total * tip;
+        withTip = justTip + total;
 
-            }
+
+        System.out.println("Total bill before tip: $" + total);
+        System.out.println("Total percentage: " + percent + "%");
+        System.out.println("Total tip: $" + justTip);
+        System.out.println("Total bill with tip: $" + withTip);
 
         }
     }
